79c712b30ad14a35757d14099ef330a20073a89f
vasily.smolin <vasilysmoolin@gmail.com>
2022-09-21 17:15:07 +0400

    i18n

--- src/init.js
+++ src/init.js
@@ -1,5 +1,5 @@
 import 'bootstrap';
-import { object, string } from 'yup';
+import { object, string, setLocale } from 'yup';
 import axios from 'axios';
 import i18next from 'i18next';
 import watch from './watchers.js';
@@ -7,53 +7,65 @@ import watch from './watchers.js';
 export default () => {
 
     // создание экземпляра i18next
-    // const i18nextInstance = i18next.createInstance();
-    // i18nextInstance.init({
-    //     lng: 'ru',
-    //     body: {
-    //       'h1': 'RSS агрегатор',
-    //     },
-    //     form: {
-    //         'url': ''
-    //     },
-    // });
-
-    const state = {
-        form: {
-            state: 'valid',
-            data: {
-                url: '',
+    const i18n = i18next.createInstance();
+    i18n.init({
+        lng: 'ru',
+        debug: true,
+        resources: {
+            ru: {
+                translation: {
+                    errors: {
+                        required: 'Не должно быть пустым',
+                        notUrl: 'Ссылка должна быть валидным URL',
+                    },
+                }
+            }
+        }
+    }).then(() => {
+        setLocale({
+            string: {
+                url: () => ({ key: 'notUrl' }),
             },
-            error: null,
-        },
-    };
+            mixed: {
+                required: () => ({ key: 'required' }),
+            },
+        });
 
-    const elements = {
-        from: document.querySelector('.rss-form'),
-        textDanger: document.querySelector('.text-danger'),
-        url: document.querySelector('[aria-label="url"]'),
-        add: document.querySelector('[aria-label="add"]'),
-    }
 
-    let userSchema = object({
-        url: string().url(),
+        const state = {
+            form: {
+                state: 'valid',
+                data: {
+                    url: '',
+                },
+                error: null,
+            },
+        };
+
+        const elements = {
+            from: document.querySelector('.rss-form'),
+            textDanger: document.querySelector('.text-danger'),
+            url: document.querySelector('[aria-label="url"]'),
+            add: document.querySelector('[aria-label="add"]'),
+        }
+
+        let userSchema = object({
+            url: string().url().required(),
+        });
+
+        const watchedState = watch(elements, state, i18n);
+
+        elements.add.addEventListener('click', function (e) {
+            e.preventDefault();
+            userSchema.validate({url: elements.url.value},{ abortEarly: false })
+                .then(() => null)
+                .catch((e) => {
+                    watchedState.form = {
+                        ...watchedState.form,
+                        state: 'invalid',
+                        error: e.message,
+                    };
+                });
+        });
     });
-
-    const watchedState = watch(elements, state);
-
-    elements.add.addEventListener('click', function (e) {
-        e.preventDefault();
-        userSchema.validate({url: elements.url.value},{ abortEarly: false })
-            .then(() => null)
-            .catch((e) => {
-                watchedState.form = {
-                    ...watchedState.form,
-                    state: 'invalid',
-                    error: e.message,
-                };
-            });
-    });
-
-
-
 };
--- src/watchers.js
+++ src/watchers.js
@@ -1,13 +1,12 @@
 import onChange from 'on-change';
 
-export default (elements, state) => {
-    const watchState = onChange(state, (path, value) => {
+export default (elements, state, i18n) => {
+    return onChange(state, (path, value) => {
         if (path === 'form') {
             if (state.form.state === 'invalid') {
-                elements.textDanger.textContent = value.error;
+                elements.textDanger.textContent = i18n.t([`errors.${value.error.key}`]);
             }
         }
     });
-    return watchState;
 };
 
